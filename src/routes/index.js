23b62f6292e4841b16ca6b2a3c4ca3c05fa33653
adam dong <adam.yh.dong@foxconn.com>
2017-03-18 17:13:32 +0800

    Only admin can view groups page.

--- src/routes/index.js
+++ src/routes/index.js
@@ -115,6 +115,7 @@ module.exports = function (app, middleware, hotswapIds) {
 	app.all(relativePath + '(/api|/api/*?)', middleware.prepareAPI);
 	app.all(relativePath + '(/api/admin|/api/admin/*?)', middleware.isAdmin);
 	app.all(relativePath + '(/admin|/admin/*?)', ensureLoggedIn.ensureLoggedIn(nconf.get('relative_path') + '/login?local=1'), middleware.applyCSRF, middleware.isAdmin);
+	app.all(relativePath + '(/groups)', ensureLoggedIn.ensureLoggedIn(nconf.get('relative_path') + '/login?local=1'), middleware.applyCSRF, middleware.isAdmin);
 
 	app.use(middleware.maintenanceMode);
 
